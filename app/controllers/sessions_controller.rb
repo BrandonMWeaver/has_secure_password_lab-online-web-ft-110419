b3f715eed6b825921002ef220b3d804f5c7bacdf
Brandon Weaver <a7xncob@gmail.com>
2020-02-04 23:23:32 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,26 @@ class SessionsController < ApplicationController
     @user = User.new
   end
   
+  #def create
+  #  user = User.find_by(name: params[:name])
+  #  user = user.try(:authenticate, params[:password])
+  #  return redirect_to new_session_path unless user
+  #  session[:user_id] = user.id
+  #  redirect_to welcome_path
+  #end
+  
   def create
     user = User.find_by(name: params[:name])
+
     user = user.try(:authenticate, params[:password])
-    return redirect_to new_session_path unless user
+
+    return redirect_to(controller: 'sessions', action: 'new') unless user
+
     session[:user_id] = user.id
-    redirect_to welcome_path
+
+    @user = user
+
+    redirect_to controller: 'welcome', action: 'index'
   end
   
   def destroy
