c8550c200a6d71c64835f1fce085c5096dd3cca6
Brandon Weaver <a7xncob@gmail.com>
2020-02-04 23:23:24 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,25 +4,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
-  #def create
-  #  user = User.create(user_params)
-  #  return redirect_to new_user_path unless user.save
-  #  session[:user_id] = user.id
-  #  redirect_to welcome_path
-  #end
-  
   def create
-    user = User.find_by(name: params[:name])
-
-    user = user.try(:authenticate, params[:password])
-
-    return redirect_to(controller: 'sessions', action: 'new') unless user
-
+    user = User.create(user_params)
+    return redirect_to new_user_path unless user.save
     session[:user_id] = user.id
-
-    @user = user
-
-    redirect_to controller: 'welcome', action: 'index'
+    redirect_to welcome_path
   end
   
   private
